d9e61b51023cff1e7f31d0b81c3e44c5b9a022ef
Anik <aniketandy@gmail.com>
2018-10-06 23:54:54 +0530

    changes

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ const apiRoutes = require('./server/routes');
 const dbInit = require('./server/utilities/dbInit.js');
 const sequalize = dbInit.sequalize;
 
+var corsResolver = function(res){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    return res;
+}
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.set('port', (process.env.PORT || 3300));
 app.use(cors());
 app.use(bodyParser.json());
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,13 +1,26 @@
 const productService = require('../services/productService');
 
 let ProductController = function(){
+    
 }
+
+ProductController.prototype.corsResolver = function(res){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    return res;
+}
+
+
 
 ProductController.prototype.addProduct = function(req,res){    
     let promise = productService.addProduct(req.body);
     promise.then(result => {
+       // res = this.corsResolver(res);
+        
         res.json(result);
     },err =>{
+        
         res.status(err);
         res.json(err);
     });    
